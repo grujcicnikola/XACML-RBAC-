dc57366fe9a516807760a6ef71b96e8d53ac763a
grujcicnikola <grujcicnikola@uns.ac.rs>
2021-05-23 13:07:02 +0200

    dialogs or policy and rule

--- frontend/src/app/policy/policy.component.ts
+++ frontend/src/app/policy/policy.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PolicySet } from '../model/PolicySet';
+import { UserService } from '../service/userService/user.service';
+import { TokenStorageService } from '../auth/token-storage.service';
+import { Policy } from '../model/Policy';
 
 @Component({
   selector: 'app-policy',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  private policy = new Policy();
+  @Output() closeEvent = new EventEmitter<void>();
+  //private loginInfo: LoginInfo;
 
+  constructor(private userService: UserService, private tokenStorage: TokenStorageService,
+    private formBuilder: FormBuilder) { 
+      this.form = this.formBuilder.group({
+        xsi: ['', [Validators.minLength(3), Validators.required]],
+        policyId: ['', [Validators.minLength(3), Validators.required]],
+        version: ['', [Validators.minLength(3), Validators.required]],
+        ruleCombiningAlgId: ['', [Validators.minLength(3), Validators.required]],
+        description: ['', [Validators.minLength(3), Validators.required]],
+        schemaLocator: ['', [Validators.minLength(3), Validators.required]]
+      });
+    }
+   
   ngOnInit() {
+    
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    // this.userService.register(this.user).subscribe(res => {
+    //   window.location.href = "https://localhost:4200";
+    // }, err => {
+
+    // });
   }
 
 }
--- frontend/src/app/rule/rule.component.ts
+++ frontend/src/app/rule/rule.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PolicySet } from '../model/PolicySet';
+import { UserService } from '../service/userService/user.service';
+import { TokenStorageService } from '../auth/token-storage.service';
+import { Rule } from '../model/Rule';
 
 @Component({
   selector: 'app-rule',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RuleComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  private rule = new Rule();
+  @Output() closeEvent = new EventEmitter<void>();
+  //private loginInfo: LoginInfo;
 
+  constructor(private userService: UserService, private tokenStorage: TokenStorageService,
+    private formBuilder: FormBuilder) { 
+      this.form = this.formBuilder.group({
+        ruleId: ['', [Validators.minLength(3), Validators.required]],
+        effect: ['', [Validators.minLength(3), Validators.required]],
+        description: ['', [Validators.minLength(3), Validators.required]]
+      });
+    }
   ngOnInit() {
+    
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    // this.userService.register(this.user).subscribe(res => {
+    //   window.location.href = "https://localhost:4200";
+    // }, err => {
+
+    // });
   }
 
 }
